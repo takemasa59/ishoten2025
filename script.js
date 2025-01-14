d2a0861c6e3a661072e66581572478e9ef596ab7
masukei1 <108469598+masukei1@users.noreply.github.com>
2025-01-14 15:55:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset; // 現在のスクロール量
   const documentHeight = document.documentElement.scrollHeight - window.innerHeight; // ドキュメントの高さ
   const scrollFraction = scrollTop / documentHeight; // スクロール量の割合
-  const frame = Math.min(scrollFraction * 1200, 970); // 0〜100%の範囲に正規化
+  const frame = Math.min(scrollFraction * 970, 970); // 0〜100%の範囲に正規化
   player.seek(frame); // フレームを指定してアニメーション位置を更新
 });
