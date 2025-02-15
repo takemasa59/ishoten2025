6390a47d89a087149c2cf583ea157a8e9aa4d8b9
masukei1 <masukeimassan@gmail.com>
2025-02-15 11:39:27 +0900

    20250215_masuda_諸々調節

--- lottie.js
+++ lottie.js
@@ -1,11 +1,13 @@
 // プレイヤーの参照を取得
 const player = document.getElementById('lottie-player');
 
+const totalFrames = 2400;
+
 // スクロールイベントでアニメーションを制御
 window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset; // 現在のスクロール量
   const documentHeight = document.documentElement.scrollHeight - window.innerHeight; // ドキュメントの高さ
   const scrollFraction = scrollTop / documentHeight; // スクロール量の割合
-  const frame = Math.min(scrollFraction * 1500, 1500); // 0〜100%の範囲に正規化
+  const frame = Math.min(scrollFraction * totalFrames, totalFrames); // 0〜100%の範囲に正規化
   player.seek(frame); // フレームを指定してアニメーション位置を更新
 });
